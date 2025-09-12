8e16ea2ced5a202a3d6e34c44c31a27a74a48c60
Fl-ris <florismenninga@gmail.com>
2025-09-12 21:04:56 +0200

    Naamgeving van variabelen naar Camelcase veranderd.

--- SNN/src/main/java/floris/SNN.java
+++ SNN/src/main/java/floris/SNN.java
@@ -1,33 +1,70 @@
 package floris;
 
+import java.util.Arrays;
+
+
 
 public class SNN {
+    byte potentialThreshold = -50;
+    byte restMembranePotential = -65;
+    byte membraneResistance = 10; // Weerstand van membraan in mega Ohm.
 
-    int potential_threshold = 60;
-    int rest_membrane_potential = 50;
-    double initial_membrane_potential = 0.9;
-    int membrane_leak = 5; // 5 ms
+    double initialMembranePotential = 60;
+    double dv;
+    double v;
+
+    int membraneLeak = 10; // 10 ms
     int neurons = 5;
 
     double dt = 0.1;
+    int simulationTime = 1;
+    double simSteps = simulationTime / dt;
 
-    int simulation_time = 1;
-    double sim_steps = simulation_time * dt;
-
-    int[][] spike_array
-
-
+    int[][] spikeArray;
 
     double[][] test = new double[neurons][neurons];
 
 
+
     public static void main(String[] args) {
-        double dt = 0.1;
+        SNN network = new SNN();
 
-        int simulation_time = 1000;
-        double sim_steps = simulation_time * dt;
+        int[] v = new int[10];
+        Arrays.fill(v, 0);
 
-        System.out.println(sim_steps);
+        v[0] = 60;
+
+        System.out.println(v[0]);
+
+
+
+
+       // System.out.println(network.v);
+        // Delta membraan voltage:
+        //double dv = -(V-El)  * network.dt
+
+
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+
+            network.dv = ((-(network.v - network.restMembranePotential) + network.membraneResistance))
+            
+        }
+
+//            System.out.println(i);
+         //   int V = (-network.initial_membrane_potential );
+
+     //   }
+
+     //  System.out.println(network.sim_steps);
+
+
+
+
+
+
 
 
 
