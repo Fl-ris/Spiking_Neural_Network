e09cc64937ea0187997077d300378b50a68d8b76
Fl-ris <florismenninga@gmail.com>
2025-09-13 00:04:15 +0200

    LIF neuron class toegevoegd en Xchart gebruikt om de neuron output te plotten.

--- SNN/src/main/java/floris/SNN.java
+++ SNN/src/main/java/floris/SNN.java
@@ -1,13 +1,17 @@
 package floris;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-
-
+import org.knowm.xchart.*;
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.IntStream;
 
 public class SNN {
     byte potentialThreshold = -50;
     byte restMembranePotential = -65;
     byte membraneResistance = 10; // Weerstand van membraan in mega Ohm.
+    double input = 2;
 
     double initialMembranePotential = 60;
     double dv;
@@ -17,57 +21,60 @@ public class SNN {
     int neurons = 5;
 
     double dt = 0.1;
-    int simulationTime = 1;
+    int simulationTime = 1000;
     double simSteps = simulationTime / dt;
 
     int[][] spikeArray;
 
     double[][] test = new double[neurons][neurons];
 
-
+    public SNN(){
+        this.v = initialMembranePotential;
+    }
 
     public static void main(String[] args) {
         SNN network = new SNN();
 
-        int[] v = new int[10];
-        Arrays.fill(v, 0);
 
-        v[0] = 60;
+        Vector<Double> vList = new Vector<>();
 
-        System.out.println(v[0]);
+            for (int i = 0; i < network.simSteps; i++) {
+            network.LIFneuron(null);
+            vList.add(network.v);
+    }
 
-
-
-
-       // System.out.println(network.v);
-        // Delta membraan voltage:
-        //double dv = -(V-El)  * network.dt
-
-
-
-
-        for (int i = 0; i < 20; i++) {
-            System.out.println(i);
-
-            network.dv = ((-(network.v - network.restMembranePotential) + network.membraneResistance))
-            
+        int n = vList.size();
+        double[] yData = new double[n];
+        double[] xData = new double[n];
+        for (int i = 0; i < n; i++) {
+            yData[i] = vList.get(i);
+            xData[i] = i * network.dt;
         }
-
-//            System.out.println(i);
-         //   int V = (-network.initial_membrane_potential );
-
-     //   }
-
-     //  System.out.println(network.sim_steps);
-
-
-
-
-
-
-
-
+        plotter(xData, yData);
 
     }
+
+    public void LIFneuron(String[] args) {
+        dv = (((-v - restMembranePotential) + membraneResistance * input)
+                / membraneLeak) * dt;
+
+        v = dv + v;
+        System.out.println(v);
+    }
+
+
+    public static void plotter(double[] xData, double[] yData) {
+        XYChart chart = QuickChart.getChart(
+                "Membrane Potential (LIF neuron)",
+                "Time (ms)", "V (mV)",
+                "V(t)", xData, yData);
+
+        chart.getStyler().setLegendVisible(false);
+        chart.getStyler().setMarkerSize(4);
+
+        new SwingWrapper<>(chart).displayChart();
+    }
+
+
 
 }
